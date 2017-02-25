b9b62fc37dd8caba1e9aa493c5d838db3463f288
Isabel Ramos <iramos1226@gmail.com>
2017-02-25 10:15:19 -0600

    got meat prices to show up in DOM

--- javascript-files/dom-handler.js
+++ javascript-files/dom-handler.js
@@ -1,7 +1,7 @@
 var showPrice = document.getElementById("final-price");
 
 // Variable to hold the final price. Default to 0.
-var finalSandwichPrice = meat;
+var finalSandwichPrice = 0;
 
 // Variable to hold topping that the user selects
 var selectedTopping;
@@ -13,13 +13,17 @@ var meatChooser = document.getElementById("meat-chooser");
   A <select> element broadcasts a change event, so you listen for it
   and get the value of the topping from your augmented IIFE
 */
-meatChooser.addEventListener("change", function(event) {
+meatChooser.addEventListener("checked", function(event) {
   // Get the value chosen from the DOM
   selectedTopping = event.target.value;
-  showPrice.innerHTML += finalSandwichPrice;
+  var meatPrice = SandwichMaker.getMeatPrices(selectedTopping);
+  finalSandwichPrice += meatPrice;
+  showPrice.innerHTML = finalSandwichPrice;
 
   // Determine the price of the topping chosen
 
 
   // Add the topping to the SandwichMaker to increase the total price
 });
+
+showPrice.innerHTML = finalSandwichPrice;
--- javascript-files/meat.js
+++ javascript-files/meat.js
@@ -1,24 +1,28 @@
 // This SandwichMaker IIFE augments the original one
-var SandwichMaker = (function(maker) {
+var SandwichMaker = (function(oldSandwichMaker) {
 
-// Private variable to store the different meat prices
-	var meatPrices = {
-		"Ham": 0.90,
-		"Turkey": 0.90, 
-		"Capicola": 1.10, 
-		"Salami": 1.00,
-		"Pepperoni": 1.10,
-		"Bacon": 1.50
-  	};
+  // Private variable to store the different meat prices
+  var meatPrices = {
+  	"Ham": 0.90,
+  	"Turkey": 0.90, 
+    "Capicola": 1.10, 
+  	"Salami": 1.00,
+  	"Pepperoni": 1.10,
+  	"Bacon": 1.50
+  }
 
-// Augment the original object with another method
-	maker.addMeat = function() {
-		console.log();
-		return meatPrices;
-	};
+  // Augment the original object with another method
+	oldSandwichMaker.getMeatPrices = function(key) {
+		return meatPrices[key];
+	}
+	// oldSandwichMaker.setMeatPrices = function() {
+	// 	return meatPrices[key]++;
+	// }
+  
 
+  oldSandwichMaker.addMeat = function() {
+  };
 
-
-// Return the new, augmented object with the new method on it
-		return maker;
+  // Return the new, augmented object with the new method on it
+  return oldSandwichMaker;
 })(SandwichMaker || {});
